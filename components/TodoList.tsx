26aeaa578b630d4537f268282da3efa74b8f459f
sangock <senseoki@gmail.com>
2022-01-25 08:09:54 +0900

    ADD: delete

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -4,7 +4,7 @@ import { TodoType } from "../types/todo";
 import palette from "../styles/palette";
 import TrashCanIcon from "../public/static/svg/trash_can.svg"
 import CheckMarkIcon from "../public/static/svg/check_mark.svg"
-import { checkTodoAPI } from "../lib/api/todo";
+import { checkTodoAPI, deleteTodoAPI } from "../lib/api/todo";
 import { useRouter } from "next/dist/client/router";
 
 
@@ -167,6 +167,17 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
     }
   };
 
+  const deleteTodo = async (id: number) => {
+    try {
+      await deleteTodoAPI(id);
+      const newTodos = localTodos.filter(todo => todo.id !== id);
+      setLocalTodos(newTodos);
+      console.log("삭제했습니다.");
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   return (
     <Container>
       <div className="todo-list-header">
@@ -201,6 +212,7 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
                   <TrashCanIcon
                     className="todo-trash-can"
                     onClick={() => {
+                      deleteTodo(todo.id);
                     }}
                   />
                   <CheckMarkIcon
